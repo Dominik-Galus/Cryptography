1ae9fb1d5e1ce52ced93f8c483f1cb16febcd9e6
Domino <dominikgalus9@gmail.com>
2024-07-13 12:17:36 +0200

    Restructured program elements

--- number.py
+++ number.py
@@ -1,32 +1,43 @@
-from restriction import Restriction
+from restrictions.restriction import Restriction
+from restrictions.ring import Ring
 
 class Number:
-    def __init__(self, value:int, restriction: Restriction) -> None:
+    def __init__(self, value: int, restriction: Restriction) -> None:
         self.restriction = restriction
-        self.restriction.check(value)
-        self.value = value
+        self.value = self.restriction.check(value)
         
     def __add__(self, other: "Number") -> "Number":
-        if self.restriction.ring_modulo() != other.restriction.ring_modulo():
-            raise ValueError("Numbers can be added with the same restriction")
-        return Number(self.restriction.add(
-            self.value, other.value
-        ), self.restriction)
-    
+        if not (type(self.restriction) == type(other.restriction) and self.restriction.modulo() == other.restriction.modulo()):
+            raise ValueError("Numbers can be added only with the same restriction")
+        return Number(
+            self.restriction.add(self.value, other.value), self.restriction
+        )
+        
     def __mul__(self, other: "Number") -> "Number":
-        if self.restriction.ring_modulo() != other.restriction.ring_modulo():
-            raise ValueError("Numbers can be multipilied with the same restriction")
-        return Number(self.restriction.mult(
-            self.value, other.value
-        ), self.restriction)
+        if self.restriction.mul(0,0) == NotImplemented or other.restriction.mul(0,0) == NotImplemented:
+            raise ValueError("Can't make multiplication with given restrictions")
+        if not (type(self.restriction) == type(other.restriction) and self.restriction.modulo() == other.restriction.modulo()):
+            raise ValueError("Numbers can be multiplied only with the same restriction")
+        return Number(
+            self.restriction.mul(self.value, other.value), self.restriction
+        )
         
-        
-n1 = Number(4, Restriction(5))
-n2 = Number(3, Restriction(5))
-n3 = n1 + n2
-n4 = n1 * n2
-n5 = (n1 + n3) * n4
+n1 = Number(3, Ring(5))
+n2 = Number(2, Ring(7))
 
-assert n3.value == 2
-assert n4.value == 2
-assert n5.value == 2
+try:
+    n3 = n1+n2
+    print(n3.value)
+except ValueError as e:
+    print(e)
+    
+n4 = Number(6, Ring(7))
+n5 = n2 + n4
+assert n5.value == 1
+n6 = Number(1, Ring(5))
+n7 = n1 + n6
+assert n7.value == 4
+n8 = n2 * n4
+assert n8.value == 5
+n9 = (n2 + n4) * n8
+assert n9.value == 5
--- restrictions/field.py
+++ restrictions/field.py
@@ -1,7 +1,7 @@
-from ring import N_Ring
+from restrictions.ring import Ring
 from pure import is_prime
 
-class N_Field(N_Ring):
+class Field(Ring):
     
     def __init__(self, mod: int) -> None:
         super().__init__(mod)
@@ -10,12 +10,11 @@ class N_Field(N_Ring):
         
     def division(self, a: int, b: int) -> int:
         b_inverse: int = self.mult_inverse(b)
-        return self.multiplication([a, b_inverse])
+        return self.mul(a, b_inverse)
         
         
-Z_7 = N_Field(7)
+Z_7 = Field(7)
 
 assert Z_7.additive_inverse(2) == 5
 assert Z_7.mult_inverse(3) == 5
 assert Z_7.division(3, 4) == 6
-assert Z_7.operation(['add', 'add', 'mult', 'add'], [7, 4, 3, 2, 9]) == 2
