cd495ed798303a159f29d3d8bc85b9b8528b707e
Domino <dominikgalus9@gmail.com>
2024-07-13 16:51:08 +0200

    Changed the standard prime number finding to fermat prime probability test

--- pure.py
+++ pure.py
@@ -1,8 +1,26 @@
 from math import sqrt
+from random import randint
 
 
-def is_prime(value: int) -> bool:
-    for i in range(2, int(sqrt(value)) + 1):
-        if value % i == 0:
-            return False
+def is_prime(value: int, k: int) -> bool:
+    if value == 1:
+        return False
+    elif value == 2 or value == 3:
+        return True
+    
+    else:
+        for i in range(k):
+            a = randint(2, value - 2)
+            if pow(a, value - 1, value) != 1:
+                return False
     return True
+
+if __name__ == "__main__":
+    k = 100
+    assert is_prime(11, k) == True
+    assert is_prime(15, k) == False
+    assert is_prime(21, k) == False
+    assert is_prime(23, k) == True
+    assert is_prime(4, k) == False
+    assert is_prime(10000023, k) == False
+    assert is_prime(234123751237, k) == True
