ec975ecba4d3e38c6471ef32d878bc8683f4570c
Domino <dominikgalus9@gmail.com>
2024-10-08 22:04:15 +0200

    Minor changes

--- src/service/server2.py
+++ src/service/server2.py
@@ -1,32 +1,34 @@
 import socket
 import threading
 
+
 class Server:
-    def __init__(self, host: str, port: int) -> None:
-        self.host: str = host
-        self.port: int = port
+    def __init__(self, address: tuple[str, int]) -> None:
+        self.host: str
+        self.port: int
+        self.host, self.port = address
         self.server_connection: socket.socket = None
         self.server_socket: socket.socket = None
         self.start_server()
         self.sessions: list[socket.socket] = []
-        
+
     def start_server(self) -> None:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen()
         print(f"Server is listening on {self.host}:{self.port}")
-        
-    def connection_handler(self, host: str, port: int) -> None:
+
+    def connection_handler(self, address: tuple[str, int]) -> None:
         connecting_socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+
         try:
-            print(f"Trying to connect to {host}:{port}...")
-            connecting_socket.connect((host, port))
+            print(f"Trying to connect to {address[0]}:{address[1]}...")
+            connecting_socket.connect((address[0], address[1]))
             self.server_connection = connecting_socket
             self.server_connection.sendall(f"{self.port}".encode())
             threading.Thread(target=self.receive_data_from_server).start()
             print("Connected with server")
-            
+
             while True:
                 sc, sockname = self.server_socket.accept()
                 response = sc.recv(1024).decode()
@@ -34,25 +36,25 @@ class Server:
                     print(f"Connected with session at {sockname}")
                     self.sessions.append(sc)
                     threading.Thread(target=self.receive_data_from_session, args=(sc,)).start()
-                    
+
         except:
-            print(f"Failed to connect to: {host}:{port}...")
+            print(f"Failed to connect to: {address[0]}:{address[1]}...")
             print("Waiting for server to connect...")
-            
+
             while True:
                 sc, sockname = self.server_socket.accept()
                 response = sc.recv(1024).decode()
-                if sockname[0] in ["localhost", "127.0.0.1"] and response == str(port):
+                if response == str(address[1]):
                     self.server_connection = sc
                     print("Connected with the server")
-                    
+
                     threading.Thread(target=self.receive_data_from_server).start()
                 else:
                     print(f"Connected with session at {sockname}")
                     self.sessions.append(sc)
-                    
+
                     threading.Thread(target=self.receive_data_from_session, args=(sc,)).start()
-                    
+
     def receive_data_from_session(self, session: socket.socket) -> None:
         while True:
             try:
@@ -66,14 +68,14 @@ class Server:
                 self.sessions.remove(session)
                 session.close()
                 break
-                
+
     def forward_data_to_server(self, message) -> None:
         try:
             self.server_connection.send(message.encode())
             print(f"Forwarded message to connected server: {message}")
         except Exception as e:
             print(f"An error occurred forwarding a data to server: ({e})")
-    
+
     def receive_data_from_server(self) -> None:
         while True:
             try:
@@ -85,7 +87,7 @@ class Server:
                 print(f"An error occurred while receiving data from server: ({e})")
                 self.server_connection.close()
                 break
-    
+
     def broadcast_to_sessions(self, message: str, sender_session: socket.socket) -> None:
         for session in self.sessions:
             if session != sender_session:
@@ -95,15 +97,16 @@ class Server:
                     print(f"An error occurred while broadcasting to sessions: ({e})")
                     self.sessions.remove(session)
                     session.close()
-                    
+
     def stop_server(self) -> None:
         self.server_socket.close()
         if self.server_connection:
             self.server_connection.close()
-                    
+
 if __name__ == "__main__":
-    server = Server("localhost", 55556)
+    server = Server(("localhost", 55556))
     try:
-        server.connection_handler("localhost", 55555)
-    except:
+        server.connection_handler(("localhost", 55555))
+    except Exception as e:
+        print(f"Server stopped: {e}")
         server.stop_server()
